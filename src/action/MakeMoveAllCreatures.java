0340f3e31a337f24e62f5571b7e68425c306284d
fearux <bekov.tembulatt@gmail.com>
2024-10-21 22:09:39 +0300

    feat: add a class for all creatures to make a move

--- src/action/MakeMoveAllCreatures.java
+++ src/action/MakeMoveAllCreatures.java
@@ -1,4 +1,17 @@
 package action;
 
-public class MakeMoveAllCreatures {
+import entity.creature.Creature;
+import field.Field;
+
+import java.util.List;
+
+public class MakeMoveAllCreatures implements Action {
+
+    @Override
+    public void execute(Field field) {
+        List<Creature> creatures = field.getAllCreaturesOnField();
+        for (Creature creature : creatures) {
+            creature.makeMove(field);
+        }
+    }
 }
