632b3e571e5349a29e3ad502b4949b0b88583f9d
fearux <bekov.tembulatt@gmail.com>
2024-10-21 22:07:22 +0300

    feat: add class which represents the coordinates from a row and a column
    use for keys in the map of the playing field

--- src/field/Coordinates.java
+++ src/field/Coordinates.java
@@ -1,5 +1,7 @@
-package entity;
+package field;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Coordinates {
@@ -38,5 +40,37 @@ public class Coordinates {
     @Override
     public int hashCode() {
         return Objects.hash(row, column);
+    }
+
+    private static boolean isValidCoordinates(Field field, Coordinates coordinates) {
+        if (coordinates.getRow() < 0 || coordinates.getRow() >= field.getHeight()) {
+            return false;
+        }
+        if (coordinates.getColumn() < 0 || coordinates.getColumn() >= field.getWidth()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static List<Coordinates> getAvailableCoordinates(Field field, Coordinates coordinates) {
+        List<Coordinates> availableCoordinates = new ArrayList<>();
+
+        int row = coordinates.getRow();
+        int column = coordinates.getColumn();
+
+        if (Coordinates.isValidCoordinates(field, new Coordinates(row + 1, column))) {
+            availableCoordinates.add(new Coordinates(row + 1, column));
+        }
+        if (Coordinates.isValidCoordinates(field, new Coordinates(row - 1, column))) {
+            availableCoordinates.add(new Coordinates(row - 1, column));
+        }
+        if (Coordinates.isValidCoordinates(field, new Coordinates(row, column + 1))) {
+            availableCoordinates.add(new Coordinates(row, column + 1));
+        }
+        if (Coordinates.isValidCoordinates(field, new Coordinates(row, column - 1))) {
+            availableCoordinates.add(new Coordinates(row, column - 1));
+        }
+
+        return availableCoordinates;
     }
 }
