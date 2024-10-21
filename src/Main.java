549a92fc45fc4c7795870c4cdcd3a845739f5e52
fearux <bekov.tembulatt@gmail.com>
2024-10-21 22:18:56 +0300

    feat: initial Main class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import field.ConsoleRenderer;
+import field.Field;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Field field = new Field(10, 30);
+        ConsoleRenderer consoleRenderer = new ConsoleRenderer();
+        Simulation simulation = new Simulation(field, consoleRenderer);
+        simulation.startSimulation();
     }
 }
