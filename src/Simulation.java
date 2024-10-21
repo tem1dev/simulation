ceed7795f1ca8a9cece5b67d2cf1211092fc985e
fearux <bekov.tembulatt@gmail.com>
2024-10-21 22:18:37 +0300

    feat: add a simulation runner class

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,2 +1,40 @@
+import field.ConsoleRenderer;
+import field.Field;
+import action.*;
+
 public class Simulation {
+    private static final int INITIAL_VALUE_OF_STEPS = 0;
+    private static final String FINAL_TEXT = "Simulation end.";
+    private final Action randomPlaceEntitiesOnField = new RandomPlaceEntitiesOnField();
+    private final Action makeMoveAllCreatures = new MakeMoveAllCreatures();
+    private Field field;
+    private ConsoleRenderer consoleRenderer;
+    private int steps;
+
+    public Simulation(Field field, ConsoleRenderer consoleRenderer) {
+        this.field = field;
+        this.consoleRenderer = consoleRenderer;
+        this.steps = INITIAL_VALUE_OF_STEPS;
+    }
+
+    public void startSimulation() throws InterruptedException {
+        randomPlaceEntitiesOnField.execute(field);
+        while (field.containsTurtles()) {
+            showInformationAboutSteps();
+            nextTurn();
+            Thread.sleep(1200);
+        }
+        consoleRenderer.render(field);
+        System.out.println(FINAL_TEXT);
+    }
+
+    public void nextTurn() {
+        consoleRenderer.render(field);
+        makeMoveAllCreatures.execute(field);
+        steps++;
+    }
+
+    public void showInformationAboutSteps() {
+        System.out.println("Step: " + steps);
+    }
 }
