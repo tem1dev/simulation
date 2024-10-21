1a84b7c02a14fe467732642c7b961c03aa77b253
fearux <bekov.tembulatt@gmail.com>
2024-10-21 22:09:02 +0300

    feat: add an abstract class for implementing subclasses of entities moving on the map

--- src/entity/creature/Creature.java
+++ src/entity/creature/Creature.java
@@ -1,15 +1,45 @@
-package entity;
+package entity.creature;
+
+import entity.Entity;
+import field.Coordinates;
+import field.Field;
 
 public abstract class Creature extends Entity {
     private final int speed;
-    private final int health;
+    private int health;
     private final Class<? extends Entity> food;
+    private Coordinates coordinates;
 
-    public Creature(int speed, int health, Class<? extends Entity> food) {
+    public Creature(int speed, int health, Class<? extends Entity> food, Coordinates coordinates) {
         this.speed = speed;
         this.health = health;
         this.food = food;
+        this.coordinates = coordinates;
     }
 
-    public abstract void makeMove();
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Class<? extends Entity> getFood() {
+        return food;
+    }
+
+    public abstract void makeMove(Field field);
 }
