cb38ed7f60d98c1a7dde3ff8b6e50af36198f82c
fearux <bekov.tembulatt@gmail.com>
2024-10-21 22:14:06 +0300

    feat: add a class representing a turtle that's a shark victim and a mobile object that eats seafeeds

--- src/entity/creature/Turtle.java
+++ src/entity/creature/Turtle.java
@@ -1,16 +1,40 @@
-package entity;
+package entity.creature;
+
+import entity.Entity;
+import entity.stationary.Sea;
+import entity.stationary.Seaweed;
+import field.Coordinates;
+import field.Field;
+import search.PathFinder;
+
+import java.util.List;
 
 public class Turtle extends Creature {
-    private static final int SPEED = 3;
-    private static final int HEALTH = 80;
+    private static final int SPEED = 2;
+    private static int HEALTH = 120;
+    private static final int BONUS_FOR_EATING = 20;
     private static final Class<? extends Entity> FOOD = Seaweed.class;
 
-    public Turtle() {
-        super(SPEED, HEALTH, FOOD);
+    public Turtle(Coordinates coordinates) {
+        super(SPEED, HEALTH, FOOD, coordinates);
     }
 
     @Override
-    public void makeMove() {
-
+    public void makeMove(Field field) {
+        PathFinder pathFinder = new PathFinder();
+        for (int step = 0; step < getSpeed(); step++) {
+            List<Coordinates> pathToTarget = pathFinder.get(field, getCoordinates(), FOOD);
+            if (pathToTarget.size() < 2) {
+                return;
+            }
+            Coordinates nextStep = pathToTarget.get(1);
+            Entity currentEntity = field.getEntity(nextStep);
+            if (currentEntity.getClass().isAssignableFrom(getFood())) {
+                setHealth(getHealth() + BONUS_FOR_EATING);
+            }
+            field.setEntity(new Sea(), getCoordinates());
+            setCoordinates(nextStep);
+            field.setEntity(this, nextStep);
+        }
     }
 }
