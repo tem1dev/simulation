ddad99855bb737bdef43cf0c831508485d60f5a8
fearux <bekov.tembulatt@gmail.com>
2024-10-21 22:13:16 +0300

    feat: add class representing the moving object shark, a predator whose target is a turtle.

--- src/entity/creature/Shark.java
+++ src/entity/creature/Shark.java
@@ -1,17 +1,43 @@
-package entity;
+package entity.creature;
+
+import entity.Entity;
+import entity.stationary.Sea;
+import field.Coordinates;
+import field.Field;
+import search.PathFinder;
+
+import java.util.List;
 
 public class Shark extends Creature {
-    private static final int SPEED = 2;
-    private static final int HEALTH = 100;
+    private static final int SPEED = 4;
+    private static final int HEALTH = 200;
     private static final Class<? extends Entity> FOOD = Turtle.class;
     private static final int ATTACK = 20;
 
-    public Shark() {
-        super(SPEED, HEALTH, FOOD);
+    public Shark(Coordinates coordinates) {
+        super(SPEED, HEALTH, FOOD, coordinates);
     }
 
     @Override
-    public void makeMove() {
-
+    public void makeMove(Field field) {
+        PathFinder pathFinder = new PathFinder();
+        for (int step = 0; step < getSpeed(); step++) {
+            List<Coordinates> pathToTarget = pathFinder.get(field, getCoordinates(), FOOD);
+            if (pathToTarget.size() < 2) {
+                return;
+            }
+            Coordinates nextStep = pathToTarget.get(1);
+            Entity currentEntity = field.getEntity(nextStep);
+            if (currentEntity.getClass().isAssignableFrom(getFood())) {
+                int targetHealth = ((Turtle) (currentEntity)).getHealth() - ATTACK;
+                if (targetHealth > 0) {
+                    ((Turtle) (currentEntity)).setHealth(targetHealth);
+                    return;
+                }
+            }
+            field.setEntity(new Sea(), getCoordinates());
+            setCoordinates(nextStep);
+            field.setEntity(this, nextStep);
+        }
     }
 }
