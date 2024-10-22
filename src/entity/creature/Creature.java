06d477d559916d295645f190a9e9623f196502ad
fearux <bekov.tembulatt@gmail.com>
2024-10-22 20:56:51 +0300

    feat: the total of the heirs is take out in super class, add the PathFinder field

--- src/entity/creature/Creature.java
+++ src/entity/creature/Creature.java
@@ -1,20 +1,26 @@
 package entity.creature;
 
 import entity.Entity;
-import field.Coordinates;
+import coordinates.Coordinates;
+import entity.stationary.Sea;
 import field.Field;
+import search.Search;
+
+import java.util.List;
 
 public abstract class Creature extends Entity {
     private final int speed;
     private int health;
     private final Class<? extends Entity> food;
     private Coordinates coordinates;
+    protected Search pathFinder;
 
-    public Creature(int speed, int health, Class<? extends Entity> food, Coordinates coordinates) {
+    public Creature(int speed, int health, Class<? extends Entity> food, Coordinates coordinates, Search pathFinder) {
         this.speed = speed;
         this.health = health;
         this.food = food;
         this.coordinates = coordinates;
+        this.pathFinder = pathFinder;
     }
 
     public Coordinates getCoordinates() {
@@ -41,5 +47,22 @@ public abstract class Creature extends Entity {
         return food;
     }
 
-    public abstract void makeMove(Field field);
+    public void makeMove(Field field) {
+        for (int step = 0; step < getSpeed(); step++) {
+            List<Coordinates> pathToTarget = pathFinder.execute(field, getCoordinates(), getFood());
+            if (pathToTarget.size() < 2) {
+                return;
+            }
+            Coordinates nextStep = pathToTarget.get(1);
+            Entity currentEntity;
+            if (!field.containsEntity(nextStep)) {
+                currentEntity = new Sea();
+            } else {
+                currentEntity = field.getEntity(nextStep);
+            }
+            moving(field, nextStep, currentEntity);
+        }
+    }
+
+    public abstract void moving(Field field, Coordinates nextStep, Entity currentEntity);
 }
