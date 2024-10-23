0898f7dee48b77e2e65cd59f83548730bc1202b4
fearux <bekov.tembulatt@gmail.com>
2024-10-23 18:10:59 +0300

    rename packages name

--- src/main/java/entity/stationary/Sea.java
+++ src/main/java/entity/stationary/Sea.java
@@ -1,6 +1,6 @@
-package main.java.action.entity.stationary;
+package main.java.entity.stationary;
 
-import main.java.action.entity.Entity;
+import main.java.entity.Entity;
 
 public class Sea extends Entity {
 }
