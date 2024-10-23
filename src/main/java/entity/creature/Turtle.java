ecfc7fb1e43734bd559a17ae478b94aaf7998a50
fearux <bekov.tembulatt@gmail.com>
2024-10-23 18:12:03 +0300

    rename packages name

--- src/main/java/entity/creature/Turtle.java
+++ src/main/java/entity/creature/Turtle.java
@@ -1,10 +1,11 @@
-package main.java.action.entity.creature;
+package main.java.entity.creature;
 
-import main.java.action.entity.Entity;
-import main.java.action.entity.stationary.Sea;
-import main.java.action.entity.stationary.Seaweed;
-import main.java.action.Field;
-import main.java.action.search.Search;
+import main.java.Coordinates;
+import main.java.entity.Entity;
+import main.java.entity.stationary.Sea;
+import main.java.entity.stationary.Seaweed;
+import main.java.Field;
+import main.java.search.Search;
 
 public class Turtle extends Creature {
     private static final int SPEED = 2;
@@ -12,12 +13,12 @@ public class Turtle extends Creature {
     private static final int BONUS_FOR_EATING = 20;
     private static final Class<? extends Entity> FOOD = Seaweed.class;
 
-    public Turtle(main.java.action.Coordinates coordinates, Search pathFinder) {
+    public Turtle(Coordinates coordinates, Search pathFinder) {
         super(SPEED, HEALTH, FOOD, coordinates, pathFinder);
     }
 
     @Override
-    public void moving(Field field, main.java.action.Coordinates nextStep, Entity currentEntity) {
+    public void moving(Field field, Coordinates nextStep, Entity currentEntity) {
         if (currentEntity.getClass().isAssignableFrom(getFood())) {
             setHealth(getHealth() + BONUS_FOR_EATING);
         }
