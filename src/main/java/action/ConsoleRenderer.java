05438e332fd9a04bc32b7c7b01ee5c29f2ebc125
fearux <bekov.tembulatt@gmail.com>
2024-10-23 17:28:38 +0300

    change exception handling to a simple condition with method isSquareEmpty

--- src/main/java/action/ConsoleRenderer.java
+++ src/main/java/action/ConsoleRenderer.java
@@ -1,20 +1,17 @@
-import coordinates.Coordinates;
-import entity.*;
-import entity.creature.Shark;
-import entity.creature.Turtle;
-import entity.stationary.Coral;
-import entity.stationary.Sea;
-import entity.stationary.Seaweed;
-import entity.stationary.Shell;
-import field.Field;
+package main.java.action;
 
-import java.util.NoSuchElementException;
+import main.java.action.entity.*;
+import main.java.action.entity.creature.Shark;
+import main.java.action.entity.creature.Turtle;
+import main.java.action.entity.stationary.Coral;
+import main.java.action.entity.stationary.Sea;
+import main.java.action.entity.stationary.Seaweed;
+import main.java.action.entity.stationary.Shell;
 
 public class ConsoleRenderer {
     private final String SHELL = "\uD83D\uDC1A";
     private final String SEA = "\uD83D\uDFE6";
     private final String CORAL = "\uD83E\uDEB8";
-    private final String CRAB = "\uD83E\uDD80";
     private final String SEAWEED = "\uD83C\uDF3F";
     private final String SHARK = "\uD83E\uDD88";
     private final String TURTLE = "\uD83D\uDC22";
@@ -22,11 +19,11 @@ public class ConsoleRenderer {
     public void render(Field field) {
         for (int row = 0; row < field.getHeight(); row++) {
             for (int column = 0; column < field.getWidth(); column++) {
-                try {
-                    Entity currentEntity = field.getEntity(new Coordinates(row, column));
-                    System.out.print(getSpriteForEntity(currentEntity));
-                } catch (NoSuchElementException e) {
+                Coordinates currentCoordinates = new Coordinates(row, column);
+                if (field.isEmptySquare(currentCoordinates)) {
                     System.out.print(getSpriteForEntity(new Sea()));
+                } else {
+                    System.out.print(getSpriteForEntity(field.getEntity(currentCoordinates)));
                 }
             }
             System.out.println();
@@ -47,6 +44,6 @@ public class ConsoleRenderer {
         } else if (entity instanceof Turtle) {
             return TURTLE;
         }
-        throw new IllegalArgumentException("Unknown type of entity");
+        throw new IllegalArgumentException("Unknown type of main.java.action.entity");
     }
 }
